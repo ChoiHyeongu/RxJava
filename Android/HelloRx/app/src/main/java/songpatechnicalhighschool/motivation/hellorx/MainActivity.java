3291326b73a20d5bd690807ab4f71b70690b769a
ChoiHyeongu <a11dsd123@gmail.com>
2019-05-20 23:57:40 +0900

    HelloRx

---
+++ Android/HelloRx/app/src/main/java/songpatechnicalhighschool/motivation/hellorx/MainActivity.java
@@ -0,0 +1,22 @@
+
+package songpatechnicalhighschool.motivation.hellorx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Observable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.text_view);
+        Observable.just(textView.getText().toString())
+                .map(s -> s + "Rx!")
+                .subscribe(text -> textView.setText(text));
+    }
+}
