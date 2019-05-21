6a62992cb2469ed7302cb148d6020fc116be494e
ChoiHyeongu <a11dsd123@gmail.com>
2019-05-21 23:54:33 +0900

    Observer, filter

--- Android/HelloRx/app/src/main/java/songpatechnicalhighschool/motivation/hellorx/MainActivity.java
+++ Android/HelloRx/app/src/main/java/songpatechnicalhighschool/motivation/hellorx/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +22,35 @@ public class MainActivity extends AppCompatActivity {
                 .map(s -> s + "Rx!")
                 .subscribe(text -> textView.setText(text));
 
+//        Observable
+        //               .just(1, 2, 3, 4, 5)
+        //             .filter(new Func1<Integer, Boolean>() {
+        //                  @Override
+        //               public Boolean call(Integer integer) {
+        //                 return integer % 2 != 0;
+        //           }
+        //     });
+
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("New data received : " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("Error received : " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("All data emitted.");
+            }
+        };
     }
 }
